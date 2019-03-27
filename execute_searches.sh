1bd547527f182b9d557ece4d72d07cde02b14343
mjhutchinson <hutchinson.michael.john@gmail.com>
2019-03-27 00:17:11 +0000

    Algorithm version 0.2.1. Specific actual data experiments to run on hinton

--- execute_searches.sh
+++ execute_searches.sh
@@ -6,39 +6,20 @@ DATADIR="data"
 OUTDIR="output"
 datasets=("power-plant" "bostonHousing" "concrete" "energy" "kin8nm")
 acquisitions=("EI" "PI" "SD")
-seeds=("0" "1" "2" "3" "4" "5")
-function_seed=9
+seeds=("0" "1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "12" "13" "14" "15" "16")
 experiment="weight_pruning_hyperprior3"
 initial_random_point=3
+thompson_samples=1
+
 
 for ds in "${datasets[@]}"; do
 
     for seed in "${seeds[@]}"; do
-            python dummy_search.py --data "$ds" --experiment "$experiment" --seed "$seed" --random --final --datadir "$DATADIR" --outdir "$OUTDIR"
+            python dummy_search.py --data "$ds" --experiment "$experiment" --thompson_samples "$thompson_samples" --seed "$seed" --random --final --datadir "$DATADIR" --outdir "$OUTDIR"
 
         for acquisition in "${acquisitions[@]}"; do
-            python dummy_search.py --data "$ds" --experiment "$experiment" --seed "$seed" --acquisition "$acquisition" --final --datadir "$DATADIR" --outdir "$OUTDIR"
+            python dummy_search.py --data "$ds" --experiment "$experiment" --thompson_samples "$thompson_samples" --seed "$seed" --acquisition "$acquisition" --final --datadir "$DATADIR" --outdir "$OUTDIR"
 #            python dummy_search.py --data "$ds" --experiment "$experiment" --seed "$seed" --acquisition "$acquisition" --datadir "$DATADIR" --outdir "$OUTDIR"
         done
     done
 done
-
-
-
-#python examples/dummy_search.py --data bostonHousing --experiment weight_pruning_hyperprior3 -n 3 # --joint
-#python examples/dummy_search.py --data concrete --experiment weight_pruning_hyperprior3 -n 3 # --joint
-#python examples/dummy_search.py --data energy --experiment weight_pruning_hyperprior3 -n 3 # --joint
-#python examples/dummy_search.py --data kin8nm --experiment weight_pruning_hyperprior3 -n 3 # --joint
-#python examples/dummy_search.py --data power-plant --experiment weight_pruning_hyperprior3 -n 3 # --joint
-#python examples/dummy_search.py --data protein-tertiary-structure --experiment weight_pruning_hyperprior3 -n 3 # --joint
-#python examples/dummy_search.py --data wine-quality-red --experiment weight_pruning_hyperprior3 -n 3 # --joint
-#python examples/dummy_search.py --data yacht --experiment weight_pruning_hyperprior3 -n 3 # --joint
-#
-#python examples/dummy_search.py --data bostonHousing --experiment weight_pruning_hyperprior3 --rmse -n 3 # --joint
-#python examples/dummy_search.py --data concrete --experiment weight_pruning_hyperprior3 --rmse -n 3 # --joint
-#python examples/dummy_search.py --data energy --experiment weight_pruning_hyperprior3 --rmse -n 3 # --joint
-#python examples/dummy_search.py --data kin8nm --experiment weight_pruning_hyperprior3 --rmse -n 3 # --joint
-#python examples/dummy_search.py --data power-plant --experiment weight_pruning_hyperprior3 --rmse -n 3 # --joint
-#python examples/dummy_search.py --data protein-tertiary-experiment --experiment weight_pruning_hyperprior3 --rmse -n 3 # --joint
-#python examples/dummy_search.py --data wine-quality-red --experiment weight_pruning_hyperprior3 --rmse -n 3 # --joint
-#python examples/dummy_search.py --data yacht --experiment weight_pruning_hyperprior3 --rmse -n 3 # --joint
