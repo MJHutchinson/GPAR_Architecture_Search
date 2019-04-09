d9ce1fed06ae04a5682a79b87c05cd8633aa6e16
mjhutchinson <hutchinson.michael.john@gmail.com>
2019-04-09 09:48:50 +0100

    Algorithm version 0.2.2. Experiment config

--- execute_searches.sh
+++ execute_searches.sh
@@ -10,16 +10,18 @@ seeds=("0" "1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "12" "13" "14" "15" "16
 experiment="weight_pruning_hyperprior3"
 initial_random_point=3
 thompson_samples=1
+samples_per_thompson=1
+name="1_sample_per_thompson"
 
 
 for ds in "${datasets[@]}"; do
 
     for seed in "${seeds[@]}"; do
-            python dummy_search.py --data "$ds" --experiment "$experiment" --thompson_samples "$thompson_samples" --seed "$seed" --random --final --datadir "$DATADIR" --outdir "$OUTDIR"
+            python dummy_search.py --name "$name"  --data "$ds" --experiment "$experiment" --seed "$seed" --thompson_samples "$thompson_samples" --samples_per_thompson "$samples_per_thompson" --random --final --datadir "$DATADIR" --outdir "$OUTDIR"
 
         for acquisition in "${acquisitions[@]}"; do
-            python dummy_search.py --data "$ds" --experiment "$experiment" --thompson_samples "$thompson_samples" --seed "$seed" --acquisition "$acquisition" --final --datadir "$DATADIR" --outdir "$OUTDIR"
-#            python dummy_search.py --data "$ds" --experiment "$experiment" --seed "$seed" --acquisition "$acquisition" --datadir "$DATADIR" --outdir "$OUTDIR"
+            python dummy_search.py --name "$name" --data "$ds" --experiment "$experiment" --seed "$seed" --thompson_samples "$thompson_samples" --samples_per_thompson "$samples_per_thompson" --acquisition "$acquisition" --final --datadir "$DATADIR" --outdir "$OUTDIR"
+#            python dummy_search.py --data "$ds" --experiment "$experiment" --function_seed "$function_seed" --noise 0.02 --seed "$seed" --thompson_samples "$thompson_samples" --acquisition "$acquisition" --datadir "$DATADIR" --outdir "$OUTDIR"
         done
     done
 done
