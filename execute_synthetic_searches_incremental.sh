985b7428c6600fcd4db75a4a3b3b1b8a7c1df1a9
mjhutchinson <hutchinson.michael.john@gmail.com>
2019-05-16 12:18:06 +0100

    Updates to requirements

--- execute_synthetic_searches_incremental.sh
+++ execute_synthetic_searches_incremental.sh
@@ -14,15 +14,15 @@ initial_random_point=5
 thompson_samples=4
 samples_per_thompson=50
 name="50_sample_per_thompson-no_refit"
-synthetic_scales=" 2. 0.5 "
+synthetic_scales="2. 0.5"
 
 for ds in "${datasets[@]}"; do
 
     for seed in "${seeds[@]}"; do
-            python incremental_search.py --name "$name"  --data "$ds" --experiment "$experiment" --function_seed "$function_seed" --synthetic_scales "$synthetic_scales" --noise 0.01 --seed "$seed" --thompson_samples "$thompson_samples" --samples_per_thompson "$samples_per_thompson" --random --datadir "$DATADIR" --outdir "$OUTDIR"
+            python incremental_search.py --name "$name"  --data "$ds" --experiment "$experiment" --function_seed "$function_seed" --noise 0.01 --seed "$seed" --thompson_samples "$thompson_samples" --samples_per_thompson "$samples_per_thompson" --random --datadir "$DATADIR" --outdir "$OUTDIR"
 
         for acquisition in "${acquisitions[@]}"; do
-            python incremental_search.py --name "$name" --data "$ds" --experiment "$experiment" --function_seed "$function_seed" --synthetic_scales "$synthetic_scales" --noise 0.01 --seed "$seed" --thompson_samples "$thompson_samples" --samples_per_thompson "$samples_per_thompson" --acquisition "$acquisition" --datadir "$DATADIR" --outdir "$OUTDIR"
+            python incremental_search.py --name "$name" --data "$ds" --experiment "$experiment" --function_seed "$function_seed" --noise 0.01 --seed "$seed" --thompson_samples "$thompson_samples" --samples_per_thompson "$samples_per_thompson" --acquisition "$acquisition" --datadir "$DATADIR" --outdir "$OUTDIR"
 #            python dummy_search.py --data "$ds" --experiment "$experiment" --function_seed "$function_seed" --noise 0.02 --seed "$seed" --thompson_samples "$thompson_samples" --acquisition "$acquisition" --datadir "$DATADIR" --outdir "$OUTDIR"
         done
     done
