normalisation fixes

incremental_search.py
@@ -238,7 +238,7 @@ print('   max:', max_layers)
 # Normalise data.
 y_means = y.mean(axis=0, keepdims=True)
 y_stds = y.std(axis=0, keepdims=True)
-y = (y - y_means) / y_stds
+y_normalised = (y - y_means) / y_stds
 
 # Define the function to unnormalise y data
 def unnormalise(y):
@@ -323,7 +323,7 @@ if not args.random:
         print('\t Fitting model')
 
         x_data = x.copy()
-        y_data = y.copy()
+        y_data = y_normalised.copy()
         y_data[np.where(~y_tested)] = np.nan
         active_inds = np.where(y_tested[:, 0])
         x_data = x_data[active_inds]
@@ -441,7 +441,7 @@ if not args.random:
         # update the best points found so far. TODO: Maybe look at making f/x_best the best from the GPAR function, rather than the data point?
         y_temp = y.copy()
         y_temp[~y_tested] = -np.inf
-        x_best, y_best = x[y_temp[:, -1].argmax()], y[y_temp[:, -1].argmax(), -1]
+        x_best, y_best = x[y_temp[:, -1].argmax()].copy(), y[y_temp[:, -1].argmax(), -1].copy()
 
         iteration_results.append(iteration_result)
 

search_analysis_incremental.py
@@ -82,100 +82,100 @@ dirs = match_dirs
 
 experiments = [Experiment(pickle.load(open(os.path.join(outdir, dir, 'config.pkl'), 'rb')), pickle.load(open(os.path.join(outdir, dir, 'results.pkl'), 'rb'))) for dir in dirs]
 
-# experiment_types = defaultdict(list)
-#
-# dump_dict = {}
-#
-# for experiment in experiments:
-#     experiment_types[(experiment.config.acquisition if not experiment.config.random else "Random", experiment.config.final)].append(experiment.results)
-#
-# colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan']
-#
-# fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=plotting_config.full_width_square)
-# plt.suptitle(f'Search methods on {args.data} comparison \n Searching for optimal {"RMSE" if args.rmse else "Log Likelihood"}')
-# ax1.set_title('Mean best search value found')
-# ax2.set_title('Standard deviation of best values found')
-#
-# for idx, key in enumerate(experiment_types.keys()):
-#     results = experiment_types[key]
-#     if results is not []:
-#         iteration_results = np.zeros([len(results[0]), len(results)])
-#         iterations = np.zeros([len(results[0]), len(results)])
-#         for i in range(len(results[0])):
-#             for j in range(len(results)):
-#                 iteration_result = np.squeeze(results[j][i].y_best)
-#                 if len(iteration_result.shape) == 2:
-#                     iteration_results[i, j] = iteration_result[:, -1]
-#                 elif len(iteration_result.shape) == 1:
-#                     iteration_results[i, j] = iteration_result[-1]
-#                 else:
-#                     iteration_results[i, j] = iteration_result
-#
-#                 iterations[i, j] = results[j][i].y_tested.shape[0]
-#
-#         # print(iterations, iteration_results)
-#
-#         # This meaning does not work if the results come at different numbers of points acquired for this search type
-#         ax1.plot(iterations[:, 0], np.mean(iteration_results, axis=1), c=colors[idx], label=f'{key[0]} {"final only" if key[1] else "all"}')
-#         ax2.plot(iterations[:, 0], np.std(iteration_results, axis=1), c=colors[idx], label=f'{key[0]} {"final only" if key[1] else "all"}')
-#
-#         dump_dict[key] = {
-#             'iterations': iterations[:, 0],
-#             'mean': list(np.mean(iteration_results, axis=1)),
-#             'std': list(np.std(iteration_results, axis=1))
-#         }
-#
-#         # plt.fill_between(iterations[:, 0], np.min(iteration_results, axis=1), np.max(iteration_results, axis=1), color=colors[idx], alpha=0.3)
-#
-#         # plt.plot(iterations[:, 0], np.min(iteration_results, axis=1), c=colors[idx], linestyle='dashed')
-#         # plt.plot(iterations[:, 0], np.max(iteration_results, axis=1), c=colors[idx], linestyle='dashed')
-#
-#         # plt.plot(iterations[:, 0], np.mean(iteration_results, axis=1) + np.std(iteration_results, axis=1), c=colors[idx], linestyle='dashed')
-#         # plt.plot(iterations[:, 0], np.mean(iteration_results, axis=1) - np.std(iteration_results, axis=1), c=colors[idx], linestyle='dashed')
-#         #
-#         # plt.fill_between(iterations[:, 0],
-#         #                  np.mean(iteration_results, axis=1) + np.std(iteration_results, axis=1),
-#         #                  np.mean(iteration_results, axis=1) - np.std(iteration_results, axis=1),
-#         #                  color=colors[idx], alpha=0.2)
-#
-# plt.legend()
-# plotting_config.savefig(outdir + '/search_results')
-# plt.close()
-#
-# pickle.dump({'name': args.name, 'data': dump_dict}, open(os.path.join(outdir, 'combined_results.pkl'), 'wb'))
-#
-# seed_dict = defaultdict(list)
-#
-# for experiment in experiments:
-#     seed_dict[experiment.config.seed].append(experiment)
-#
-# for seed in seed_dict.keys():
-#     results = seed_dict[seed]
-#
-#     if results is not []:
-#         fig, ax = plt.subplots(1, 1, figsize=plotting_config.full_width_square)
-#         ax.set_title(f'Seed {seed}')
-#
-#         for j, result in enumerate(results):
-#             iteration_results = np.zeros([len(result.results)])
-#             iterations = np.zeros([len(result.results)])
-#
-#             for i in range(len(result.results)):
-#                 iteration_result = np.squeeze(result.results[i].y_best)
-#                 if len(iteration_result.shape) == 2:
-#                     iteration_results[i] = iteration_result[:, -1]
-#                 elif len(iteration_result.shape) == 1:
-#                     iteration_results[i] = iteration_result[-1]
-#                 else:
-#                     iteration_results[i] = iteration_result
-#
-#                 iterations[i] = result.results[i].y_tested.shape[0]
-#
-#             plt.plot(iterations, iteration_results, c=colors[j], label=f'{result.config.acquisition} {"final only" if result.config.final else "all"}')
-#
-#         plt.legend()
-#         plotting_config.savefig(outdir + f'/search_results_{seed}')
-#         plt.close()
+experiment_types = defaultdict(list)
+
+dump_dict = {}
+
+for experiment in experiments:
+    experiment_types[(experiment.config.acquisition if not experiment.config.random else "Random", experiment.config.final)].append(experiment.results)
+
+colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan']
+
+fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=plotting_config.full_width_square)
+plt.suptitle(f'Search methods on {args.data} comparison \n Searching for optimal {"RMSE" if args.rmse else "Log Likelihood"}')
+ax1.set_title('Mean best search value found')
+ax2.set_title('Standard deviation of best values found')
+
+for idx, key in enumerate(experiment_types.keys()):
+    results = experiment_types[key]
+    if results is not []:
+        iteration_results = np.zeros([len(results[0]), len(results)])
+        iterations = np.zeros([len(results[0]), len(results)])
+        for i in range(len(results[0])):
+            for j in range(len(results)):
+                iteration_result = np.squeeze(results[j][i].y_best)
+                if len(iteration_result.shape) == 2:
+                    iteration_results[i, j] = iteration_result[:, -1]
+                elif len(iteration_result.shape) == 1:
+                    iteration_results[i, j] = iteration_result[-1]
+                else:
+                    iteration_results[i, j] = iteration_result
+
+                iterations[i, j] = results[j][i].y_tested_flags.sum()
+
+        # print(iterations, iteration_results)
+
+        # This meaning does not work if the results come at different numbers of points acquired for this search type
+        ax1.plot(iterations[:, 0], np.mean(iteration_results, axis=1), c=colors[idx], label=f'{key[0]} {"final only" if key[1] else "all"}')
+        ax2.plot(iterations[:, 0], np.std(iteration_results, axis=1), c=colors[idx], label=f'{key[0]} {"final only" if key[1] else "all"}')
+
+        dump_dict[key] = {
+            'iterations': iterations[:, 0],
+            'mean': list(np.mean(iteration_results, axis=1)),
+            'std': list(np.std(iteration_results, axis=1))
+        }
+
+        # plt.fill_between(iterations[:, 0], np.min(iteration_results, axis=1), np.max(iteration_results, axis=1), color=colors[idx], alpha=0.3)
+
+        # plt.plot(iterations[:, 0], np.min(iteration_results, axis=1), c=colors[idx], linestyle='dashed')
+        # plt.plot(iterations[:, 0], np.max(iteration_results, axis=1), c=colors[idx], linestyle='dashed')
+
+        # plt.plot(iterations[:, 0], np.mean(iteration_results, axis=1) + np.std(iteration_results, axis=1), c=colors[idx], linestyle='dashed')
+        # plt.plot(iterations[:, 0], np.mean(iteration_results, axis=1) - np.std(iteration_results, axis=1), c=colors[idx], linestyle='dashed')
+        #
+        # plt.fill_between(iterations[:, 0],
+        #                  np.mean(iteration_results, axis=1) + np.std(iteration_results, axis=1),
+        #                  np.mean(iteration_results, axis=1) - np.std(iteration_results, axis=1),
+        #                  color=colors[idx], alpha=0.2)
+
+plt.legend()
+plotting_config.savefig(outdir + '/search_results')
+plt.close()
+
+pickle.dump({'name': args.name, 'data': dump_dict}, open(os.path.join(outdir, 'combined_results.pkl'), 'wb'))
+
+seed_dict = defaultdict(list)
+
+for experiment in experiments:
+    seed_dict[experiment.config.seed].append(experiment)
+
+for seed in seed_dict.keys():
+    results = seed_dict[seed]
+
+    if results is not []:
+        fig, ax = plt.subplots(1, 1, figsize=plotting_config.full_width_square)
+        ax.set_title(f'Seed {seed}')
+
+        for j, result in enumerate(results):
+            iteration_results = np.zeros([len(result.results)])
+            iterations = np.zeros([len(result.results)])
+
+            for i in range(len(result.results)):
+                iteration_result = np.squeeze(result.results[i].y_best)
+                if len(iteration_result.shape) == 2:
+                    iteration_results[i] = iteration_result[:, -1]
+                elif len(iteration_result.shape) == 1:
+                    iteration_results[i] = iteration_result[-1]
+                else:
+                    iteration_results[i] = iteration_result
+
+                iterations[i] = result.results[i].y_tested_flags.sum()
+
+            plt.plot(iterations, iteration_results, c=colors[j], label=f'{result.config.acquisition} {"final only" if result.config.final else "all"}')
+
+        plt.legend()
+        plotting_config.savefig(outdir + f'/search_results_{seed}')
+        plt.close()
 
 
 if args.full:
@@ -185,13 +185,13 @@ if args.full:
 
         ## make incremental gif
 
-        images = [img for img in os.listdir(fig_dir) if img]
+        # images = [img for img in os.listdir(fig_dir) if img]
 
-        for iteration in tqdm(experiment.results, desc='Looping iterations'):
+        for iteration in tqdm(experiment.results, desc=f'Looping iterations: {experiment.config.outdir.split("/")[-1]}'):
             if iteration.x_test is None:
                 continue
 
-            # plotting.plot_iteration_incremental(**iteration._asdict(), fig_dir=fig_dir)
+            plotting.plot_iteration_incremental(**iteration._asdict(), fig_dir=fig_dir)
             pass
 
         f_bests = np.squeeze([np.squeeze(result.y_best) for result in experiment.results])